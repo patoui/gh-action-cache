e1a72db2f820e9e436395531c0cab7799d75075e
Patrique Ouimet <patrique.ouimet@gmail.com>
2023-11-03 11:51:09 -0400

    chore: testing date file creation

--- app/Console/Commands/JsonStore.php
+++ app/Console/Commands/JsonStore.php
@@ -2,28 +2,40 @@
 
 namespace App\Console\Commands;
 
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
 
 class JsonStore extends Command
 {
-    protected $signature = 'json:store {name}';
+    protected $signature = 'json:store {date}';
 
     protected $description = 'Generates a JSON file to test GitHub action store';
 
     public function handle(): int
     {
-        $filePath = sprintf('foo/%s.json', $this->argument('name'));
+        $ts = strtotime($this->argument('date'));
+
+        if ($ts === false) {
+            $this->line('Unable to parse "date" argument');
+
+            return 1;
+        }
+
+        $filePath = sprintf(
+            'foo/%s.json',
+            $date = Carbon::createFromTimestamp($ts)->format('Y-M-d')
+        );
 
         if (Storage::exists($filePath)) {
-            $this->line('FILE ALREADY EXIST');
+            $this->line(sprintf('File "%s" already exist', $filePath));
         } else {
-            $this->line('Creating new file...');
+            $this->line(sprintf('Creating file named: %s', $filePath));
             Storage::put(
                 $filePath,
-                json_encode(['foo' => 'bar'], JSON_PRETTY_PRINT)
+                json_encode(['date' => $date], JSON_PRETTY_PRINT)
             );
-            $this->line('Successfully created a new file');
+            $this->line(sprintf('Successfully created a file named: %s', $filePath));
         }
 
         return 0;
